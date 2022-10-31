34aee14f8663d8190989b743cd98ce235696eefb
23nazaryan <23vahe@rambler.ru>
2022-10-31 19:52:34 +0400

    functional components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,82 +1,73 @@
-import React from "react";
+import {useState} from "react";
 
-class Search extends React.Component {
-    state = {
-        types: [
-            {key: 'all', name: 'All'},
-            {key: 'movie', name: 'Movie'},
-            {key: 'series', name: 'Series'},
-            {key: 'episode', name: 'Episode'}
-        ],
-        type: 'all',
-        search: ''
-    }
+const Search = props => {
+    const {
+        searchMovies = Function.prototype
+    } = props
 
-    handleSearch = () => {
-        let {search, type} = this.state
+    const types = [
+        {key: 'all', name: 'All'},
+        {key: 'movie', name: 'Movie'},
+        {key: 'series', name: 'Series'},
+        {key: 'episode', name: 'Episode'}
+    ]
 
+    const [type, setType] = useState('all')
+    const [search, setSearch] = useState('')
+
+    const handleSearch = () => {
         if (search.length < 4) {
             alert('The search term is too short')
             return false
         }
 
-        this.props.searchMovies(search, type)
+        searchMovies(search, type)
     }
 
-    handleFilter = event => {
-        let {search} = this.state
-        let type = event.target.value
-
-        this.setState(() => ({type}), () => {
-            this.props.searchMovies(search, type)
-        })
+    const handleFilter = event => {
+        setType(event.target.value)
+        searchMovies(search, event.target.value)
     }
 
-    render() {
-        const {types, search, type} = this.state
-
-        return (<div className="row">
-            <div className="input-field col s12">
-                <div className="row">
-                    <div className="col s2 m1">
-                        <a className="btn-floating btn-large waves-effect waves-light red"
-                           onClick={this.handleSearch}
-                        ><i className="fa-solid fa-magnifying-glass"></i>
-                        </a>
-                    </div>
-                    <div className="col s10 m11">
-                        <input
-                            id="email"
-                            type="search"
-                            className="validate"
-                            placeholder="Search"
-                            value={search}
-                            onChange={e => {
-                                this.setState({search: e.target.value})
-                            }}
-                        />
-                    </div>
+    return (<div className="row">
+        <div className="input-field col s12">
+            <div className="row">
+                <div className="col s2 m1">
+                    <a className="btn-floating btn-large waves-effect waves-light red"
+                       onClick={handleSearch}
+                    ><i className="fa-solid fa-magnifying-glass"></i>
+                    </a>
+                </div>
+                <div className="col s10 m11">
+                    <input
+                        id="email"
+                        type="search"
+                        className="validate"
+                        placeholder="Search"
+                        value={search}
+                        onChange={e => setSearch(e.target.value)}
+                    />
                 </div>
             </div>
-            {
-                types.map(item => (<div className="col s3" key={item.key}>
-                    <p>
-                        <label htmlFor={'id_' + item.key}>
-                            <input
-                                id={'id_' + item.key}
-                                name="type"
-                                type="radio"
-                                value={item.key}
-                                checked={item.key === type}
-                                onChange={this.handleFilter}
-                            />
-                            <span>{item.name}</span>
-                        </label>
-                    </p>
-                </div>))
-            }
-        </div>)
-    }
+        </div>
+        {
+            types.map(item => (<div className="col s3" key={item.key}>
+                <p>
+                    <label htmlFor={'id_' + item.key}>
+                        <input
+                            id={'id_' + item.key}
+                            name="type"
+                            type="radio"
+                            value={item.key}
+                            checked={item.key === type}
+                            onChange={handleFilter}
+                        />
+                        <span>{item.name}</span>
+                    </label>
+                </p>
+            </div>))
+        }
+    </div>)
 }
 
 export default Search
